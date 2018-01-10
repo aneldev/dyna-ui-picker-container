e730b7f8ddee3518d4b8ff66b8dc0fdf66cee6a2
Dennis <dynasdaq@gmail.com>
2018-01-10 21:45:34 +0100

    --amend

---
+++ dist/src/DynaPickerContainer.d.ts
@@ -0,0 +1,23 @@
+import * as React from "react";
+import "./style.less";
+import "./color.less";
+export declare enum EStyle {
+    ROUNDED = "INLINE_ROUNDED",
+}
+export declare enum EColor {
+    WHITE_BLACK = "WHITE_BLACK",
+    WHITE_RED = "WHITE_RED",
+    BLACK_WHITE = "BLACK_WHITE",
+    TRANSPARENT_WHITE = "TRANSPARENT_WHITE",
+    ORANGE_WHITE = "ORANGE_WHITE",
+}
+export interface IDynaPickerContainerProps {
+    show?: boolean;
+    children: any;
+    style?: EStyle;
+    color?: EColor;
+}
+export declare class DynaPickerContainer extends React.Component<IDynaPickerContainerProps> {
+    static defaultProps: IDynaPickerContainerProps;
+    render(): JSX.Element;
+}
