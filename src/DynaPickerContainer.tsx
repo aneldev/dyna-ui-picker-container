820b37206e8ed18c11a1332f5547ac74d0440016
dennisat <dennis@anel.co>
2019-06-04 19:40:33 +0200

    Feature: point the pointer respectivelly the content

--- src/DynaPickerContainer.tsx
+++ src/DynaPickerContainer.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {guid} from "dyna-guid";
 import {EColor} from "dyna-ui-styles";
 
 import "./style.less";
@@ -12,29 +13,28 @@ export {EColor}
 
 export interface IDynaPickerContainerProps {
   show?: boolean;
-  children: any;
+  children: JSX.Element;
   style?: EStyle;
   color?: EColor;
   responsive?: boolean;   // Set it to true and in tablet breakpoint will occupy the whole screen.
 }
 
 export class DynaPickerContainer extends React.Component<IDynaPickerContainerProps> {
-  static defaultProps: IDynaPickerContainerProps = {
+  static defaultProps: Partial<IDynaPickerContainerProps> = {
     show: true,
-    children: null,
     style: EStyle.ROUNDED,
     color: EColor.WHITE_BLACK,
     responsive: true,
   };
 
+  private readonly id = `id--${guid()}`;
+  private readonly containerRef = React.createRef<HTMLDivElement>();
+  private readonly innerStyleRef = React.createRef<HTMLStyleElement>();
+
   constructor(props: IDynaPickerContainerProps) {
     super(props);
-    this.keepInScreen = this.keepInScreen.bind(this);
+    (window as any).point = this.pointLeft.bind(this)
   }
-
-  public refs: {
-    container: HTMLDivElement,
-  };
 
   public componentDidMount(): void {
     this.keepInScreen();
@@ -47,12 +47,44 @@ export class DynaPickerContainer extends React.Component<IDynaPickerContainerPro
 
   public componentDidUpdate(): void {
     this.keepInScreen();
+    this.pointLeft(this.getPointLeft());
   }
 
-  private keepInScreen(): void {
-    const {show} = this.props;
-    const {container} = this.refs;
+  private pointLeft(left: number): void {
+    const { show } = this.props;
+    const pickerContainer = this.containerRef.current;
     if (!show) return;
+    if (!pickerContainer) return;
+
+    const css = `
+      .${this.id}::before{ left: ${left}px; }
+      .${this.id}::after{ left: ${left}px; }
+    `;
+
+    if (this.innerStyleRef.current) this.innerStyleRef.current.innerHTML = css;
+  }
+
+  private getPointLeft = (): number => {
+    const { show } = this.props;
+    const pickerContainer = this.containerRef.current;
+    if (!pickerContainer) return -1;
+    const content: HTMLElement = pickerContainer.parentElement as HTMLElement;
+    if (!show) return -1;
+
+    const contentBCR = content.getBoundingClientRect();
+    const contentX = contentBCR.left;
+    const contentWidth = contentBCR.width;
+    const pickerBCR = pickerContainer.getBoundingClientRect();
+    const pickerX = pickerBCR.left;
+
+    return contentX - pickerX + (contentWidth / 2) - 10;
+  };
+
+  private keepInScreen = (): void => {
+    const {show} = this.props;
+    const container = this.containerRef.current;
+    if (!show) return;
+    if (!container) return;
 
     // reset the position to get the actual default value
     container.style.left = "";
@@ -83,10 +115,16 @@ export class DynaPickerContainer extends React.Component<IDynaPickerContainerPro
       `dyna-ui-picker-container--color-${color}`,
       `dyna-ui-picker-container--${show ? 'show' : 'hide'}`,
       `dyna-ui-picker-container--${responsive ? 'responsive' : ''}`,
+      this.id,
     ].filter(Boolean).join(' ');
 
     return (
-      <div className={className} ref="container">{children}</div>
+      <>
+        <style ref={this.innerStyleRef}/>
+        <div className={className} ref={this.containerRef}>
+          {children}
+        </div>
+      </>
     );
   }
 }
